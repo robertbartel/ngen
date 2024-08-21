8588274711547e52d6b8b22a855ad1393f2fe2d5
GitHub Action <runner@fv-az659-55.luykedtozraezimdvwvtsy4hga.cx.internal.cloudapp.net>
2024-08-21 13:26:20 +0000

    Commiting Documentation

--- classdata__access_1_1_data_provider.js
+++ classdata__access_1_1_data_provider.js
@@ -1,13 +1,15 @@
 var classdata__access_1_1_data_provider =
 [
-    [ "~DataProvider", "classdata__access_1_1_data_provider.html#a78cbffa63282f92cec20f13ef3067436", null ],
-    [ "finalize", "classdata__access_1_1_data_provider.html#acb736869524034e5758702e16291e518", null ],
-    [ "get_available_variable_names", "classdata__access_1_1_data_provider.html#a13ffb91d8fdba4a13ef440f7621d9cfe", null ],
-    [ "get_data_start_time", "classdata__access_1_1_data_provider.html#a9a5503cb5da0b2bf8fe25d0500bf33c4", null ],
-    [ "get_data_stop_time", "classdata__access_1_1_data_provider.html#af598b54c03163287adc56400b3e9bdfe", null ],
-    [ "get_ts_index_for_time", "classdata__access_1_1_data_provider.html#acca503f7d24367b8a3298a2c10e5d75f", null ],
-    [ "get_value", "classdata__access_1_1_data_provider.html#a2409247d93cc63d0626e623c3a024f1b", null ],
-    [ "get_values", "classdata__access_1_1_data_provider.html#adcd588f997aa5ec185744d36e801054a", null ],
-    [ "is_property_sum_over_time_step", "classdata__access_1_1_data_provider.html#a8254f58e64499bb4ae799ab120fd0934", null ],
-    [ "record_duration", "classdata__access_1_1_data_provider.html#a558af5fd091e84283470af3ca74d24d8", null ]
+    [ "data_type", "classdata__access_1_1_data_provider.html#a0d14bbf2549386ef2baefc87248ae3ad", null ],
+    [ "selection_type", "classdata__access_1_1_data_provider.html#ab0b1614239c57ead01efc4f45bf7d719", null ],
+    [ "~DataProvider", "classdata__access_1_1_data_provider.html#a05eb2e57a95517673fe2161aa45c906a", null ],
+    [ "finalize", "classdata__access_1_1_data_provider.html#af783d34fb272298f8c8f9673b1ea891e", null ],
+    [ "get_available_variable_names", "classdata__access_1_1_data_provider.html#aa466b968fd8003f2c7ea1feaa6ae39fd", null ],
+    [ "get_data_start_time", "classdata__access_1_1_data_provider.html#a1d676573f75add7dcc500158cdada40d", null ],
+    [ "get_data_stop_time", "classdata__access_1_1_data_provider.html#abd0d3178a89234d538ab2a04bf3af3ef", null ],
+    [ "get_ts_index_for_time", "classdata__access_1_1_data_provider.html#ac149b78c521ce281847f40c9f0279d8c", null ],
+    [ "get_value", "classdata__access_1_1_data_provider.html#a8c55fdbb6c7878bbea0d05ab2aaa8b39", null ],
+    [ "get_values", "classdata__access_1_1_data_provider.html#a4ced90182c881aa0c597083ab619724d", null ],
+    [ "is_property_sum_over_time_step", "classdata__access_1_1_data_provider.html#adbf7b08984e7a17e21e50db3e779a8b7", null ],
+    [ "record_duration", "classdata__access_1_1_data_provider.html#a676578fae3696947cfa0269a2135a338", null ]
 ];
